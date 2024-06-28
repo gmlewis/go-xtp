06ccb59a14b7cd2d5b7468007f241c0a6a9715ff
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-06-27 22:51:01 -0400

    Add missing files
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

---
+++ schema/testdata/hand-user.go
@@ -0,0 +1,16 @@
+package testdata // this line will be stripped in the unit test
+
+// Address represents a users address.
+type Address struct {
+	// Street address
+	Street *string `json:"street,omitempty"`
+}
+
+// User represents a user object in our system..
+type User struct {
+	// The user's age, naturally
+	Age *int `json:"age,omitempty"`
+	// The user's email, of course
+	Email   *string  `json:"email,omitempty"`
+	Address *Address `json:"address,omitempty"`
+}
