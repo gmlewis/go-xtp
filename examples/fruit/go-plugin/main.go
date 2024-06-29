f4b2a9ac440292c9a435cd19ff2930d0721b64d1
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-06-29 15:05:52 -0400

    Remove panic calls
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- examples/fruit/go-plugin/main.go
+++ examples/fruit/go-plugin/main.go
@@ -5,7 +5,6 @@ package main
 // no inputs or outputs.
 func VoidFunc() {
 	// TODO: fill out your implementation here
-	panic("VoidFunc not yet implemented.")
 }
 
 // PrimitiveTypeFunc - This demonstrates how you can accept or return primtive types.
@@ -15,14 +14,14 @@ func VoidFunc() {
 // Returns A boolean encoded as json
 func PrimitiveTypeFunc(input string) bool {
 	// TODO: fill out your implementation here
-	panic("PrimitiveTypeFunc not yet implemented.")
+	return false
 }
 
 // ReferenceTypeFunc - This demonstrates how you can accept or return references to schema types.
 // And it shows how you can define an enum to be used as a property or input/output.
 func ReferenceTypeFunc(input Fruit) ComplexObject {
 	// TODO: fill out your implementation here
-	panic("ReferenceTypeFunc not yet implemented.")
+	return ComplexObject{}
 }
 
 func main() {}
