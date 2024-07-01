fc147bef87d4203860dcc210ff2c7e176afa9527
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-06-30 21:52:07 -0400

    Work on plugin files
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- codegen/code-gen-go-plugin.go
+++ codegen/code-gen-go-plugin.go
@@ -1,14 +1,29 @@
 package codegen
 
+import (
+	"bytes"
+	"html/template"
+)
+
 // GeneratedFiles represents the files in the generated code.
 type GeneratedFiles map[string]string
 
+var (
+	goPluginXtpTOMLTemplate = template.Must(template.New("cost-gen-go-plugin.go:goPluginXtpTOMLTemplateStr").Parse(goPluginXtpTOMLTemplateStr))
+)
+
 // genGoPluginPDK generates Plugin PDK code to process plugin calls in Go.
 func (c *Client) genGoPluginPDK() (GeneratedFiles, error) {
+	var buf bytes.Buffer
+	if err := goPluginXtpTOMLTemplate.Execute(&buf, c); err != nil {
+		return nil, err
+	}
+
 	m := GeneratedFiles{
 		"build.sh":               buildShScript,
-		c.CustTypesFilename:      c.CustTypes,
-		c.CustTypesTestsFilename: c.CustTypesTests,
+		c.CustTypesFilename:      "package main\n\n" + c.CustTypes,
+		c.CustTypesTestsFilename: "package main\n\n" + c.CustTypesTests,
+		"xtp.toml":               buf.String(),
 	}
 
 	return m, nil
@@ -16,4 +31,17 @@ func (c *Client) genGoPluginPDK() (GeneratedFiles, error) {
 
 var buildShScript = `#!/bin/bash -e
 xtp plugin build
+`
+
+var goPluginXtpTOMLTemplateStr = `app_id = "app_<enter-app-id-here>"
+
+# This is where 'xtp plugin push' expects to find the wasm file after the build script has run.
+bin = "{{ .PkgName }}.wasm"
+extension_point_id = "ext_<enter-extension-point-id-here>"
+name = "go-xtp-plugin-{{ .PkgName }}"
+
+[scripts]
+
+  # xtp plugin build runs this script to generate the wasm file
+  build = "tinygo build -target wasi -o {{ .PkgName }}.wasm ."
 `
--- codegen/code-gen-mbt-plugin.go
+++ codegen/code-gen-mbt-plugin.go
@@ -1,12 +1,40 @@
 package codegen
 
+import (
+	"bytes"
+	"html/template"
+)
+
+var (
+	mbtPluginXtpTOMLTemplate = template.Must(template.New("cost-gen-mbt-plugin.go:mbtPluginXtpTOMLTemplateStr").Parse(mbtPluginXtpTOMLTemplateStr))
+)
+
 // genMbtPluginPDK generates Plugin PDK code to process plugin calls in Mbt.
 func (c *Client) genMbtPluginPDK() (GeneratedFiles, error) {
+	var buf bytes.Buffer
+	if err := mbtPluginXtpTOMLTemplate.Execute(&buf, c); err != nil {
+		return nil, err
+	}
+
 	m := GeneratedFiles{
 		"build.sh":               buildShScript,
-		c.CustTypesFilename:      c.CustTypes,
-		c.CustTypesTestsFilename: c.CustTypesTests,
+		c.CustTypesFilename:      "package main\n\n" + c.CustTypes,
+		c.CustTypesTestsFilename: "package main\n\n" + c.CustTypesTests,
+		"xtp.toml":               buf.String(),
 	}
 
 	return m, nil
 }
+
+var mbtPluginXtpTOMLTemplateStr = `app_id = "app_<enter-app-id-here>"
+
+# This is where 'xtp plugin push' expects to find the wasm file after the build script has run.
+bin = "{{ .PkgName }}.wasm"
+extension_point_id = "ext_<enter-extension-point-id-here>"
+name = "mbt-xtp-plugin-{{ .PkgName }}"
+
+[scripts]
+
+  # xtp plugin build runs this script to generate the wasm file
+  build = "moon build --target wasm && cp ../../../target/wasm/release/build/examples/{{ .PkgName }}/mbt-plugin/mbt-plugin.wasm ./{{ .PkgName }}.wasm"
+`
