58eb3e4e3cf38e718dc825e7bc46b1d27e53bace
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-07-06 15:43:30 -0400

    Update example
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- examples/user/go-plugin/main.go
+++ examples/user/go-plugin/main.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/extism/go-pdk"
@@ -11,9 +12,13 @@ import (
 
 // ProcessUser - The second export function
 func ProcessUser(input User) User {
-	pdk.Log(pdk.LogDebug, fmt.Sprintf("ENTER TinyGo plugin ProcessUser('%v')", input))
+	inBuf, err := json.MarshalIndent(input, "", "  ")
+	if err != nil {
+		pdk.Log(pdk.LogError, fmt.Sprintf("MarshalIndent error: %v", err))
+	}
+	pdk.Log(pdk.LogDebug, fmt.Sprintf("ENTER TinyGo plugin ProcessUser(): input=\n%s", inBuf))
 	// TODO: fill out your implementation here
-	pdk.Log(pdk.LogDebug, fmt.Sprintf("LEAVE TinyGo plugin ProcessUser('%v')", input))
+	pdk.Log(pdk.LogDebug, "LEAVE TinyGo plugin ProcessUser")
 	age := 42
 	email := "email@example.com"
 	return User{
