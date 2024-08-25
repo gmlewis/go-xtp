fb3647eecf53e140b5aacd21e260b2cb6191072a
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-08-25 19:44:10 -0400

    Work in progress
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- codegen/code-gen-mbt-types.go
+++ codegen/code-gen-mbt-types.go
@@ -2,6 +2,7 @@ package codegen
 
 import (
 	"bytes"
+	_ "embed"
 	"errors"
 	"fmt"
 	"strings"
@@ -40,8 +41,6 @@ func (c *Client) genMbtCustomTypes() error {
 	if c.numStructs > 0 {
 		srcBlocks = append(srcBlocks, mbtXTPSchemaMap)
 	}
-
-	srcBlocks = append(srcBlocks, mbtJSONWorkaround)
 
 	src := strings.Join(srcBlocks, "\n")
 	c.CustTypesFilename = fmt.Sprintf("%v.%v", c.PkgName, c.Lang)
@@ -106,81 +105,11 @@ func (c *Client) getTestMbtEnum(ct *schema.CustomType) (string, error) {
 	return buf.String(), nil
 }
 
-var enumMbtTemplateStr = "{{ $name := .Name }}/// `{{ $name }}` represents {{ .Description | downcaseFirst | multilineComment }}." + `
-pub enum {{ $name }} {
-{{range .Enum}}  {{ . | uppercaseFirst }}
-{{ end -}}
-} derive(Show, Eq)
+//go:embed enum-mbt-template.txt
+var enumMbtTemplateStr string
 
-` + "/// `{{ $name }}.to_string` implements the Show trait." + `
-pub fn to_string(self : {{ $name }}) -> String {
-  match self {
-  {{range .Enum}}  {{ . | uppercaseFirst }} => "{{ . }}"
-  {{ end -}}
-  }
-}
-
-` + "/// `{{ $name }}::from_json` transforms a `Json` to a value." + `
-pub fn {{ $name }}::from_json(value : Json) -> {{ $name }}? {
-  match value.as_string() {
-    {{range .Enum}}Some("{{ . }}") => Some({{ . | uppercaseFirst }})
-    {{ end -}}
-    _ => None
-  }
-}
-
-` + "/// `{{ $name }}::parse` parses a JSON string and returns the value." + `
-pub fn {{ $name }}::parse(s : String) -> {{ $name }}!String {
-  match s {
-{{range .Enum}}    "\"{{ . }}\"" => {{ . | uppercaseFirst }}
-{{ end -}}
-{{ "    _ => {" }}
-      raise "not a {{ $name }}: \{s}"
-    }
-  }
-}
-
-pub impl @jsonutil.ToJson for {{ $name }} with to_json(self) {
-  match self {
-  {{range .Enum}}  {{ . | uppercaseFirst }} => @jsonutil.to_json("{{ . }}")
-  {{ end -}}
-  }
-}
-`
-
-var enumTestMbtTemplateStr = `{{ $name := .Name }}test "{{ $name }}" {
-  let first = {{ $name }}::{{ index .Enum 0 | uppercaseFirst }}
-  let got = first.to_string()
-  let want = "{{ index .Enum 0 }}"
-  assert_eq!(got, want)!
-  //
-  let want =
-    #|"{{ index .Enum 0 }}"
-  let got_parse = {{ $name }}::parse(want)!
-  assert_eq!(got_parse, first)!
-  //
-  let mut threw_error = false
-  let _ = try {
-    {{ $name }}::parse("")!
-  } catch {
-    _ => {
-      threw_error = true
-      {{ $name }}::{{ index .Enum 0 | uppercaseFirst }}
-    }
-  }
-  @test.is_true(threw_error)!
-  //
-  let json_value = @jsonutil.to_json(first)
-  let got = json_value.stringify()
-  assert_eq!(got, want)!
-  //
-  let got_parse = {{ $name }}::from_json(json_value).unwrap()
-  assert_eq!(got_parse, first)!
-  //
-  let want_none = {{ $name }}::from_json(Json::String(""))
-  assert_eq!(want_none, None)!
-}
-`
+//go:embed enum-test-mbt-template.txt
+var enumTestMbtTemplateStr string
 
 // getMbtStruct generates MoonBit source code for a single struct custom datatype.
 func (c *Client) genMbtStruct(ct *schema.CustomType) (string, error) {
@@ -207,125 +136,8 @@ var mbtXTPSchemaMap = "/// `XTPSchema` describes the values and types of an XTP 
 type XTPSchema Map[String, String]
 `
 
-var structMbtTemplateStr = "{{ $name := .Name }}{{ $top := . }}/// `{{ $name }}` represents {{ .Description | downcaseFirst }}." + `
-pub struct {{ $name }} {
-{{range .Properties}}  {{ .Description | optionalMbtMultilineComment }}{{ .Name | lowerSnakeCase }} : {{ getMbtType . }}
-{{ end -}}
-} derive(Show, Eq)
+//go:embed struct-mbt-template.txt
+var structMbtTemplateStr string
 
-` + "/// `{{ $name }}::new` returns a new struct with default values." + `
-pub fn {{ $name }}::new() -> {{ $name }} {
-  {
-{{range .Properties}}    {{ .Name | lowerSnakeCase }}: {{ defaultMbtValue . }},
-{{ end -}}
-{{ "  }" }}
-}
-
-pub impl @jsonutil.ToJson for {{ $name }} with to_json(self) {
-  let fields : Array[(String, @jsonutil.ToJson)] = [
-{{range .Properties}}{{ if .IsRequired }}    ("{{ .Name }}", self.{{ .Name | lowerSnakeCase }}),
-{{ end }}{{ end -}}
-{{ "  ]" }}
-{{range .Properties}}{{ if .IsRequired | not }}  match self.{{ .Name | lowerSnakeCase }} {
-    Some(value) => fields.append([("{{ .Name }}", value)])
-    None => ()
-  }
-{{ end }}{{ end -}}
-{{ "  @jsonutil.from_entries(fields)" }}
-}
-
-` + "/// `{{ $name }}::from_json` transforms a `Json` to a value." + `
-pub fn {{ $name }}::from_json(value : Json) -> {{ $name }}? {
-  let value = value.as_object()?
-{{range .Properties}}  let {{ .Name | lowerSnakeCase }} = {{ mbtConvertFromJSONValue . }}
-{{ end -}}
-{{ "  match (" }}
-{{range .Properties}}    {{ .Name | lowerSnakeCase }},
-{{ end -}}
-{{ "  ) {" }}{{ if len .Properties | lt 1 }}
-    ({{ end }}
-{{range .Properties}}{{ if .IsRequired }}      Some({{ .Name | lowerSnakeCase }}){{ if len $top.Properties | lt 1 }}{{ "," }}{{ end }}
-{{ end }}{{ if .IsRequired | not }}      {{ .Name | lowerSnakeCase }}{{ if len $top.Properties | lt 1 }}{{ "," }}{{ end }}
-{{ end }}{{ end -}}
-{{ if len .Properties | lt 1 }}{{"    ) => Some({" }}{{ else }}{{"    => Some({" }}{{ end }}
-{{range .Properties}}      {{ .Name | lowerSnakeCase }},
-{{ end -}}
-{{ "    })" }}
-    _ => None
-  }
-}
-
-` + "/// `{{ $name }}::parse` parses a JSON string and returns the value." + `
-pub fn {{ $name }}::parse(s : String) -> {{ $name }}!String {
-  match @json.parse(s)!! {
-    Ok(jv) =>
-      match {{ $name }}::from_json(jv) {
-        Some(value) => value
-        None => {
-          raise "unable to parse {{ $name }} \{s}"
-        }
-      }
-    Err(e) => {
-      raise "unable to parse {{ $name }} \{s}: \{e}"
-    }
-  }
-}
-
-` + "/// `get_schema` returns an `XTPSchema` for the `{{ $name }}`." + `
-pub fn get_schema(self : {{ $name }}) -> XTPSchema {
-  {
-{{range .Properties}}    "{{ .Name }}": "{{ getExtismType . $top }}",
-{{ end -}}
-{{ "  }" }}
-}
-`
-
-var structTestMbtTemplateStr = `{{ $name := .Name }}{{ $top := . }}test "{{ $name }}" {
-  let default_object = {{ $name }}::new()
-  let got = @jsonutil.to_json(default_object)
-    |> @json.stringify()
-  let want =
-{{ "    #|{" }}{{range $index, $prop := .Properties}}{{ if .IsRequired }}"{{ .Name }}":{{ defaultMbtJSONValue . $top }}{{ showJSONCommaForRequired $index $top }}{{ end }}{{ end -}}{{ "}" }}
-  assert_eq!(got, want)!
-  //
-  let got_parse = {{ $name }}::parse(want)!
-  assert_eq!(got_parse, default_object)!
-  //
-  let required_fields : {{ $name }} = {
-{{range .Properties}}    {{ .Name | lowerSnakeCase }}: {{ requiredMbtValue . }},
-{{ end -}}
-{{ "  }" }}
-  let got = @jsonutil.to_json(required_fields)
-    |> @json.stringify()
-  let want =
-{{ "    #|{" }}{{range $index, $prop := .Properties}}{{ if .IsRequired }}"{{ .Name }}":{{ requiredMbtJSONValue . $top }}{{ showJSONCommaForRequired $index $top }}{{ end }}{{ end -}}{{ "}" }}
-  assert_eq!(got, want)!
-  //
-  let got_parse = {{ $name }}::parse(want)!
-  assert_eq!(got_parse, required_fields)!
-{{ if hasOptionalFields .}}  //
-  let optional_fields : {{ $name }} = {
-    ..required_fields,
-{{range $index, $prop := .Properties}}{{ if .IsRequired | not}}    {{ .Name | lowerSnakeCase }}: {{ optionalMbtValue . }},
-{{ end }}{{ end -}}
-{{ "  }" }}
-  let got = @jsonutil.to_json(optional_fields)
-    |> @json.stringify()
-  let want =
-{{ "    #|{" }}{{ $propLen := .Properties | len }}{{range $index, $prop := .Properties}}"{{ .Name }}":{{ requiredMbtJSONValue . $top }}{{ showJSONCommaForOptional $index $propLen }}{{ end -}}{{ "}" }}
-  assert_eq!(got, want)!
-  //
-  let got_parse = {{ $name }}::parse(want)!
-  assert_eq!(got_parse, optional_fields)!
-{{ end -}}
-}
-`
-
-var mbtJSONWorkaround = `// https://github.com/moonbitlang/core/issues/651
-fn json_as_integer(value : Json) -> Int? {
-  match value.as_number() {
-    Some(n) => Some(n.to_int())
-    None => None
-  }
-}
-`
+//go:embed struct-test-mbt-template.txt
+var structTestMbtTemplateStr string
